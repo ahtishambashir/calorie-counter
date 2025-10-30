f030e52feb02e32a685c793e24ef5c833195d718
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-10-30 19:10:14 +0500

    added the reverse string mehtod

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,3 +118,13 @@ function clearForm() {
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
 clearButton.addEventListener("click", clearForm);
+
+const reverseString = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed
+}
+
+console.log(reverseString("MentorSols"));
